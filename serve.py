de45918b48877d30fe52aef9517bcbd94500b61a
lauraturner <37046209+lauraturner@users.noreply.github.com>
2020-03-03 10:54:44 -0500

    Add route to recive form data

--- serve.py
+++ serve.py
@@ -1,11 +1,16 @@
-from flask import Flask           # import flask
-from flask import render_template
+from flask import Flask, jsonify, render_template, request           # import flask
+
 
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def index():                     
     return render_template("index.html") 
+
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate():
+	data = request.form.to_dict(flat=False)
+	return data
 
 if __name__ == "__main__":        # on running python app.py
     app.run(debug=True)                     # run the flask app
--- static/box.js
+++ static/box.js
@@ -1,37 +1,46 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        var reactors = [];
-        $.each($("input[name='check[]']:checked"), function(){
-            reactors.push($(this).attr("id"));
+$(document).ready(function() {
+    // $("button").click(function(){
+    //     var reactors = [];
+    //     $.each($("input[name='check[]']:checked"), function(){
+    //         reactors.push($(this).attr("id"));
+    //     });
+    //     var start_date=$('input[name="startDate"]'); //our date input has the name "date"
+    //     var end_date=$('input[name="endDate"]');
+    //     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    //     var options={
+    //     format: 'mm/dd/yyyy',
+    //     container: container,
+    //     todayHighlight: true,
+    //     autoclose: true,
+    //     };
+    //     start_date.datepicker(options);
+    //     end_date.datepicker(options);
+    //     console.log(start_date);
+    // });
+
+
+    $("#CANDU-form").submit(function(event) {
+        event.preventDefault(); //prevent default action 
+        var post_url = $(this).attr("action"); //get form action url
+        var form_data = $(this).serialize(); //Encode form elements for submission
+
+        $.post(post_url, form_data, function(response) {
+            $("#server-results").html(response);
         });
-        var start_date=$('input[name="startDate"]'); //our date input has the name "date"
-        var end_date=$('input[name="endDate"]');
-        var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-        var options={
-        format: 'mm/dd/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-        };
-        start_date.datepicker(options);
-        end_date.datepicker(options);
     });
 
-    $(".parent").each(function(index){
+    $(".parent").each(function(index) {
         var group = $(this).data("group");
         var parent = $(this);
-    
-        parent.change(function(){  //"select all" change 
-             $(group).prop('checked', parent.prop("checked"));
+
+        parent.change(function() { //"select all" change 
+            $(group).prop('checked', parent.prop("checked"));
         });
-        $(group).change(function(){ 
+        $(group).change(function() {
             parent.prop('checked', false);
-            if ($(group+':checked').length == $(group).length ){
+            if ($(group + ':checked').length == $(group).length) {
                 parent.prop('checked', true);
             }
         });
     });
 });
-
-
-

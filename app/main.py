5a1fd4f32240ab0a45a43068477ebffbb7ee1fd4
lauraturner <37046209+lauraturner@users.noreply.github.com>
2020-03-12 12:57:12 -0400

    Add function to calculate the daily neutrino spectrum

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 import app.data_for_calc as data_for_calc
 import app.bundle_age as bundle_age
 from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+from operator import add
 
 # any data relating to the four isotopes are stored in arrays as [U235, U238, Pu239, Pu241]
 
@@ -24,27 +27,58 @@ q_err = [0.26, 0.52, 0.34, 0.33]        #MeV
 # Neutrino emission spectrum per fission per MeV 
 v_spectrum = data_for_calc.get_spectrum() 
 
-def fuel_ratio(thermal_power, bundle_age, date):
-    power_sum = 0
-    day_pwr = thermal_power.loc[thermal_power['date'] == date]
-    for age in bundle_age.keys():
-        power_frac = 0
+def power_calc(age):
+    power_frac = 0
+    for fuel in q_i.keys():
+        age_fissions = f_i.loc[f_i['days'] == int(age)]
+        power_frac += age_fissions[fuel].values[0] * q_i[fuel]
+    return power_frac
+
+def spectrum_calc(day_spectrum, age, bundle_age):
+    spec_sum = []
+    for index, row in v_spectrum.iterrows():
+        spec_frac = 0
         for fuel in q_i.keys():
             age_fissions = f_i.loc[f_i['days'] == int(age)]
-            power_frac += age_fissions[fuel].values[0] * q_i[fuel]
+            spec_frac += age_fissions[fuel].values[0] * row[fuel]
+        spec_sum.append(bundle_age[age] * spec_frac)
+    return spec_sum
+
+def calc(thermal_power, bundle_age, date):
+    power_sum = 0
+    spec_sum = np.zeros(len(energy))
+    day_pwr = thermal_power.loc[thermal_power['date'] == date]
+    for age in bundle_age.keys():
+        spectrum = spectrum_calc(day_spectrum, age, bundle_age)
+        spec_sum = list(map(add, spectrum, spec_sum))
+        power_frac = power_calc(age)
         power_sum += bundle_age[age] * power_frac
-    return day_pwr['thermal_pwr'].values[0] / power_sum
+    power_ratio = day_pwr['thermal_pwr'].values[0] / power_sum
+    spec_sum = [x * power_ratio for x in spec_sum]
+    day_spectrum['add_spec'] = spec_sum
+    day_spectrum['neutrinos'] += day_spectrum['add_spec']
+    del day_spectrum['add_spec']
+    return day_spectrum
+
+def get_spec_df():
+    energy = v_spectrum['energy_MeV'].tolist()
+    d = {'energy_MeV': energy, 'neutrinos': np.zeros(len(energy))}
+    spectrum = pd.DataFrame(data=d)
+    spectrum.set_index('energy_MeV')
+    return spectrum
 
 def main(start, end, reactors):
     start = datetime.strptime(start, "%m/%d/%Y") # data period start
     end = datetime.strptime(end, "%m/%d/%Y") # data period end
     p_th = data_for_calc.get_thermal_data(start, end, reactors) # MWh 
     delta = end - start
+    # TODO make data struc to have a df for each reactor so spectrums can be summed for each day
+    spectrum = get_spec_df()
     for i in range(delta.days + 1):
         date = start + timedelta(days=i)
         for reactor in reactors:
             bundle_ages = bundle_age.main(reactor, date, f_i)
-            pwr_ratio = fuel_ratio(p_th[reactor], bundle_ages, date)
+            day_spectrum = calc(p_th[reactor], bundle_ages, date)
 
 
 

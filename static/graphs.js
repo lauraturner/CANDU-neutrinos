e839e26c9863da80dbfad5f9463fd9391260251b
lauraturner <37046209+lauraturner@users.noreply.github.com>
2020-04-30 14:29:30 -0400

    Fix chartjs chart sizing and colour

--- static/graphs.js
+++ static/graphs.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var colours = ['rgb(169, 212, 161)', 'rgb(132, 163, 227)', 'rgb(143, 201, 194)', 'rgb(183, 169, 199)', 'rgb(106, 200, 150)', 'rgb(153, 177, 209)'];
     // get reactor data from local storage, add heading and call functions 
     // to add charts and html elements 
     var reactors = localStorage.getItem('data');
@@ -8,27 +9,26 @@ $(document).ready(function() {
     for (var i = 0; i < reactors.length; i++) {
         var reactor = reactors[i];
         addTags(reactor.reactor);
-        addCharts(reactor);
+        var colour = colours[i % 6];
+        addCharts(reactor, colour);
     }
+    $(".chartjs-hidden-iframe").remove();
 
     // Add html tags for reactor charts and titles 
     function addTags(reactorID) {
-        var link = "<a href='#head'" + reactorID + ">" + reactorID +
+        var link = "<a href='#head" + reactorID + "'>" + reactorID +
             "</a>&emsp;"
         $("#chartLink").append(link);
 
         // TODO: fix streching
-        var chartTag = "<h2 id='head'" + reactorID + ">" + reactorID + "</h2>" +
-            "<canvas id='" + reactorID + "' width=\"100\" height=\"200\"></canvas>";
+        var chartTag = "<h2 id='head" + reactorID + "'>" + reactorID + "</h2>" +
+            "<canvas id='" + reactorID + "' width='1100' height='800'></canvas>";
         $("#charts").append(chartTag);
     }
 
     // Add Chartjs chart to page for each of the reactors
-    function addCharts(reactor) {
-        ctx = document.getElementById(reactor.reactor);
-
-        ctx.width = ctx.height *
-            (ctx.clientWidth / ctx.clientHeight);
+    function addCharts(reactor, colour) {
+        ctx = document.getElementById(reactor.reactor).getContext('2d');;
 
         chart = new Chart(ctx, {
             // The type of chart we want to create
@@ -40,7 +40,7 @@ $(document).ready(function() {
             data: {
                 labels: reactor.label,
                 datasets: [{
-                    backgroundColor: 'rgb(40, 166, 99)',
+                    backgroundColor: colour,
                     borderColor: 'rgb(255, 99, 132)',
                     data: reactor.data
                 }]
@@ -50,17 +50,17 @@ $(document).ready(function() {
                 legend: {
                     display: false,
                 },
-                responsive: true,
-                maintainAspectRatio: false,
+                responsive: false,
+                maintainAspectRatio: true,
                 scales: {
                     yAxes: [{
                         scaleLabel: {
-                            fontSize: 30,
+                            fontSize: 20,
                             display: true,
                             labelString: 'No. anitneutrinos per MeV per second'
                         },
                         ticks: {
-                            fontSize: 20,
+                            fontSize: 10,
                             callback: function(value, index, values) {
                                 return value.toExponential();
                             }
@@ -68,12 +68,12 @@ $(document).ready(function() {
                     }],
                     xAxes: [{
                         scaleLabel: {
-                            fontSize: 30,
+                            fontSize: 20,
                             display: true,
                             labelString: 'Energy level [MeV]'
                         },
                         ticks: {
-                            fontSize: 15
+                            fontSize: 10
                         }
                     }],
                 }
